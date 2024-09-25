9366a1069c3e3637d4f453e2947198d43917d1f8
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 15:42:48 +0530

    c# program8

---
+++ C#/ConsoleApp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 100; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+        static bool IsPrime(int i)
+        {
+            if (i < 2)
+            {
+                return false;
+            }
+            for (int j = 2; j<= i/2; j++)
+            {
+                if (i%j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
