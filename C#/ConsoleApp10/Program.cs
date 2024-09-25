003f64f5527e01548199103e4e8888a211f440e9
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 16:10:46 +0530

    c# program10

---
+++ C#/ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string to check as palindrome : ");
+            string input_string = Console.ReadLine();
+            string reversed_string = new string(input_string.Reverse().ToArray());
+            Console.WriteLine($"{input_string} is " + (input_string.Equals(reversed_string,StringComparison.OrdinalIgnoreCase)? "a Palindrome" : "Not Palindrome"));
+        }
+    }
+}
