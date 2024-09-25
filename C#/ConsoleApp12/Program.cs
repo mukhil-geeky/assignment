5747c63fa4b561ed058c9a3fb77bb6c7cd451eaf
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 16:59:40 +0530

    c# program12

--- C#/ConsoleApp12/Program.cs
+++ C#/ConsoleApp12/Program.cs
@@ -11,29 +11,32 @@ namespace ConsoleApp12
         static void Main(string[] args)
         {
             Console.WriteLine("Input the character from the list [c,f,h,t,b] to display the coressponding sports");
-            char input = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            switch (char.ToLower(input))
-            {
-                case 'c':
-                    Console.WriteLine("Sport : Cricket");
-                    break;
-                case 'f':
-                    Console.WriteLine("Sport : Football");
-                    break;
-                case 'h':
-                    Console.WriteLine("Sport : Hockey");
-                    break;
-                case 't':
-                    Console.WriteLine("Sport : Tennis");
-                    break;
-                case 'b':
-                    Console.WriteLine("Sport : Badminton");
-                    break;
-                default:
-                    Console.WriteLine("No such sports");
-                    break;
-            }
+            label1:
+                char input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (char.ToLower(input))
+                {
+                    case 'c':
+                        Console.WriteLine("Sport : Cricket");
+                        break;
+                    case 'f':
+                        Console.WriteLine("Sport : Football");
+                        break;
+                    case 'h':
+                        Console.WriteLine("Sport : Hockey");
+                        break;
+                    case 't':
+                        Console.WriteLine("Sport : Tennis");
+                        break;
+                    case 'b':
+                        Console.WriteLine("Sport : Badminton");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please try again");
+                        goto label1;
+                }
+
+            //Console.ReadLine();
         }
     }
 }
