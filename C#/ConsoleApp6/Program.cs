308a3f663fd029fba8a8d9763301f7e50560fc67
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 13:38:23 +0530

    c# program6

---
+++ C#/ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number :");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0, temp;
+            while (number > 0)
+            {
+                temp = number % 10;
+                sum = sum + temp*temp;
+                number = number / 10;
+
+            }
+            Console.WriteLine("Sum of Squares of digits of a number = " + sum);
+        }
+    }
+}
