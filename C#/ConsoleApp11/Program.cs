5e1ea243095438f8ca58982beee12f7cc50355f4
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 16:28:39 +0530

    c# program11

---
+++ C#/ConsoleApp11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount : ");
+            int amount = int.Parse(Console.ReadLine());
+            if (amount < 10000)
+            {
+                float tax = amount * 0.5f;
+                Console.WriteLine($"Tax Amount is {tax}"); 
+            }
+            else if(amount>10000 && amount<15000)
+            {
+                float tax = amount * 7.5f;
+                Console.WriteLine($"Tax Amount is {tax}");
+            }
+            else if (amount>15000 && amount<20000)
+            {
+                float tax = amount * .1f;
+                Console.WriteLine($"Tax Amount is {tax}");
+            }
+            else if(amount>20000 && amount<25000)
+            {
+                float tax = amount * .125f;
+                Console.WriteLine($"Tax Amount is {tax}");
+            }
+            else
+            {
+                float tax = amount * .15f;
+                Console.WriteLine($"Tax Amount is {tax}");
+            }
+        } 
+    }
+}
