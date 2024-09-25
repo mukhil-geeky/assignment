881902e8ea0c21c40859d1f100baec6866b9f0f7
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 12:35:32 +0530

    c# program5

---
+++ C#/ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number :");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0,temp;
+            while (number > 0)
+            {
+                temp = number % 10;
+                sum = sum + temp;
+                number = number / 10;
+
+            }
+            Console.WriteLine("Sum of digits of number = " + sum);
+        }
+    }
+}
