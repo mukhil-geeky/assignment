44230d3aa1e9114f6408821bfc97b0fe2b370f1b
mukhil geeky <mukhil.mp@geekywolf.com>
2024-09-25 12:05:23 +0530

    c# program2

---
+++ C#/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿// PRINT ODD NUMBERS LESS THAN 50 USING WHILE LOOP
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number < 50)
+            {
+                if (number % 2 == 1)
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+        }
+    }
+}
