c871653a1fb106885431e0fc67f2f57bb49eb6af
mukhil geeky <mukhil.mp@geekywolf.com>
2024-10-07 09:48:37 +0530

    html, css, js files

---
+++ html_js_css/hello.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("alertButton");
+    button.addEventListener("click", function() {
+        alert("Hello World");
+    });
+});
